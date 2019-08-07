317a6c9000f173a69f751db3c1447c39bc58543d
Hannah Nilagate <Nilagathp@gmail.com>
2019-08-07 18:57:34 -0400

    Finished

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Dog
-  # code goes here
+attr_accessor :owner
+attr_reader :name
+@@all = []
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all<<self
+  end
+
+  def mood
+    return @mood
+  end
+
+  def mood=(new_mood)
+    @mood = new_mood
+  end
+
+  def self.all
+    return @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,61 @@
 require 'pry'
 
 class Owner
+#attr_accessor :cat, :dog
 attr_reader :name, :species
 @@all = []
   def initialize(name)
     @name = name
     @species = "human"
     @@all << self
+  end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+    #binding.pry
+  end
+
+  def feed_cats
+    self.cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    my_cats = self.cats
+    my_dogs = self.dogs
+    my_pets = my_cats + my_dogs
+    my_pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    binding.pry
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
   def say_species
